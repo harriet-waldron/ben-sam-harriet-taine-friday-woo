93fd5acd14edbb0d0cde618055c3bc31c6a406af
ben-chamberlain <ben.albrecht.chamberlain@gmail.com>
2021-06-11 11:19:12 +1200

    server running

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ server.use(express.urlencoded({ extended: false }))
 
 // Your routes/router(s) should go heres
 server.get('/', (req, res) => {
-  res.sendFile('./index.html')
+  res.sendFile(__dirname + '/index.html')
 })
 
 module.exports = server
