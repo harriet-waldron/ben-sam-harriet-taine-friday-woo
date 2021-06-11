8833f454ac55fb15d89c08bbbbc35f41ebb007eb
Harriet Waldron <74028338+harriet-waldron@users.noreply.github.com>
2021-06-11 12:59:37 +1200

    database functions

--- vowelGame.js
+++ vowelGame.js
@@ -2,8 +2,6 @@ vowelGame = document.getElementById("vowelGame")
 vowelGame.addEventListener("submit", get_input)
 vowelGame.style.display = 'none'
 
-
-
 function get_input(e){
     const answer = document.getElementById("vowelAnswer").value;
     console.log('answer', answer)
